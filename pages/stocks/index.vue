6baffb981def882be675566fdc0d1faaccccfcb9
Ste1509 <esteffany.3801921726@ucaldas.edu.co>
2023-11-17 14:13:41 -0500

    pages purchases, stocks and pos->product

---
+++ pages/stocks/index.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <JcLoader :load="load"></JcLoader>
+    <AdminTemplate :page="page" :modulo="modulo">
+      <div slot="body">
+        <div class="row justify-content-end">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-body">
+                <table class="table">
+                  <thead>
+                    <th class="py-0 px-1">#</th>
+                    <th class="py-0 px-1">NOMBRE</th>
+                    <th class="py-0 px-1">CODIGO BARRAS</th>
+                    <th class="py-0 px-1">MARCA</th>
+                    <th class="py-0 px-1">CATEGORIA</th>
+                    <th class="py-0 px-1">STOCK</th>
+                    <th class="py-0 px-1">INVERSION</th>
+                    <th class="py-0 px-1">VALORIZADO</th>
+                    <th class="py-0 px-1">GANANCIA</th>
+                    <th class="py-0 px-1"></th>
+                  </thead>
+                  <tbody>
+                    <tr v-for="(m, i) in list">
+                      <td class="py-0 px-1">{{ i + 1 }}</td>
+                      <td class="py-0 px-1">{{ m.name}}</td>
+                      <td class="py-0 px-1">{{ m.barcode }}</td>
+                      <td class="py-0 px-1">{{ m.brand.name}}</td>
+                      <td class="py-0 px-1">{{ m.category.name }}</td>
+                      <td class="py-0 px-1">
+                      <span class="badge " :class="[m.stock<m.minimum_stock?'badge-danger':'badge-success']"
+                      >{{ m.stock }} {{ m.measurement.code }} </span>
+                    </td>
+                      <td class="py-0 px-1">{{ Number( m.investment).toFixed(2) }}</td>
+                      <td class="py-0 px-1">{{ Number( m.valued).toFixed(2) }}</td>
+                      <td class="py-0 px-1">{{ Number( m.gain).toFixed(2) }}</td>
+                      <td class="py-0 px-1">
+                        <div class="btn-group">
+                          <nuxtLink
+                            :to="url_modify + m.id"
+                            class="btn btn-info btn-sm py-1 px-2"
+                          >
+                            <i class="fas fa-eye"></i>
+                          </nuxtLink>
+
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </AdminTemplate>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: this.modulo,
+    };
+  },
+
+  data() {
+    return {
+      load: true,
+      list: [],
+      apiUrl: 'stocks',/**modificar apiUrl: /stocks */
+      page: "Inventario",
+      modulo: "General",
+      url_modify: "/stocks/kardex/",
+    };
+  },
+  methods: {
+    async getData(path) {
+      const res = await this.$api.$get(path);
+      return res;
+    },
+    async EliminarItem(id) {
+      this.load = true;
+      try {
+        const res = await this.$api.$delete(this.apiUrl + "/" + id);
+        console.log(res);
+        await Promise.all([this.getData(this.apiUrl)]).then((v) => {
+          this.list = v[0];
+        });
+      } catch (e) {
+        console.log(e);
+      } finally {
+        this.load = false;
+      }
+    },
+    Eliminar(id) {
+      let self = this;
+      this.$swal
+        .fire({
+          title: "Deseas Eliminar?",
+          showDenyButton: false,
+          showCancelButton: true,
+          confirmButtonText: "Eliminar",
+          cancelarButtonText: `Cancelar`,
+        })
+        .then(async (result) => {
+          /* Read more about isConfirmed, isDenied below */
+          if (result.isConfirmed) {
+            await self.EliminarItem(id);
+          }
+        });
+    },
+  },
+  mounted() {
+    this.$nextTick(async () => {
+      try {
+        await Promise.all([this.getData(this.apiUrl)]).then((v) => {
+          this.list = v[0];
+        });
+      } catch (e) {
+        console.log(e);
+      } finally {
+        this.load = false;
+      }
+    });
+  },
+};
+</script>
